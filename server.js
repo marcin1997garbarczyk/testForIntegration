0e8d6152fc338cb6d00451e3d581ea90ba14fefb
marcin1997garbarczyk <marcin1997garbarczyk@gmail.com>
2020-02-28 10:09:16 +0000

    superv5

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-
+console.log('@@@MAGAR'+__dirname+'/public');
 //setting middleware
-app.use(express.static(__dirname + 'public')); //Serves resources from public folder
+app.use(express.static(__dirname + '/public')); //Serves resources from public folder
 
 
 var server = app.listen(port);
