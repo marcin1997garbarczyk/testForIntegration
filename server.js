da130af123aad41b941b16d2a62afbbd424697be
marcin1997garbarczyk <marcin1997garbarczyk@gmail.com>
2020-02-28 09:49:44 +0000

    Super

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+//setting middleware
+app.use(express.static(__dirname + 'public')); //Serves resources from public folder
+
+
+var server = app.listen(port);
