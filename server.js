a0f6ff85a4b786f02c821a4b04667358bd13b66f
marcin1997garbarczyk <marcin1997garbarczyk@gmail.com>
2020-03-06 10:14:20 +0000

    log

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 console.log('@@@MAGAR'+__dirname+'/public');
-console.log('@@@MAGAR'+process.env);
+console.log('@@@MAGAR'+JSON.stringfy(process.env));
 //setting middleware
 app.use(express.static(__dirname + '/public')); //Serves resources from public folder
 
