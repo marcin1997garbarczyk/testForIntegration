36f2db4a9695e56db387400ce4b2d3c29d9be341
marcin1997garbarczyk <marcin1997garbarczyk@gmail.com>
2020-03-06 10:00:29 +0000

    nerwy

---
+++ config.js
@@ -0,0 +1,4 @@
+var config = {}
+
+config.endpoint = "https://magardb.documents.azure.com:443/";
+config.key = "ZcAQc63TGfjr7SqdrnaC1gwfZzuLTkktBH9G7uvIUc4vtvg7oUkqO41bX3P1FmtzLZXcyXIi9Z3Njvj3uZzFlw==";
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 console.log('@@@MAGAR'+__dirname+'/public');
+console.log('@@@MAGAR'+process.env);
 //setting middleware
 app.use(express.static(__dirname + '/public')); //Serves resources from public folder
 
