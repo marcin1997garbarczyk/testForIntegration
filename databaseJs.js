ec252a40621c40dea8d1999982a2b920ef2d7607
margarbarczyk <marcin.garbarczyk@enxoo.com>
2020-03-24 19:21:03 +0100

    intergationDBMAGARCONFIG

---
+++ databaseJs.js
@@ -0,0 +1,45 @@
+  var Connection = require('tedious').Connection;
+
+  var config = {
+    server: "magarserver.database.windows.net",
+    options: {},
+    authentication: {
+      type: "default",
+      options: {  
+        userName: "magar",
+        password: "zaq1@WSX",
+      }
+    }
+  };
+
+
+  var connection = new Connection(config);
+
+  connection.on('connect', function(err) {
+    if(err) {
+      console.log('Error: ', err)
+    } else {
+      console.log('@@@MAGAR Connection success');
+      executeStatement();
+    }
+  });
+
+  var Request = require('tedious').Request;
+
+  function executeStatement() {
+    const request = new Request("select 42, 'hello world'", function(err, rowCount) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(rowCount + ' rows');
+      }
+    });
+
+    request.on('row', function(row) {
+      row.forEach(function(cell) {
+        console.log(cell.value);
+      });
+    });
+
+    connection.execSql(request);
+  }
